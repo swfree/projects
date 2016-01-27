726aeecb2fe388d3c548d958c4256e9e8ccf331b
swfree <skye.free@gmail.com>
2016-01-27 11:28:14 -0500

    Update changes made to main app.js file

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -5,10 +5,18 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// connect to local MongoDB instance:
+var mongoose = require('mongoose');
+require('./models/Posts');
+require('./models/Comments');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// mongoose stuff continued
+mongoose.connect('mongodb://localhost/news');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
