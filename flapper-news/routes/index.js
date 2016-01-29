803792cb78c648ae4d5d1e1ab19bcffe3467b71e
swfree <skye.free@gmail.com>
2016-01-29 15:31:05 -0500

    Fix funky spacing

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -14,85 +14,85 @@ module.exports = router;
 
 /* GET posts */
 router.get('/posts', function(req, res, next) {
-	Post.find(function(err, posts) {
-		if(err){ return next(err); }
+  Post.find(function(err, posts) {
+    if(err){ return next(err); }
 
-		res.json(posts);
-	});
+    res.json(posts);
+  });
 });
 
 router.post('/posts', function(req, res, next) {
-	var post = new Post(req.body);
+  var post = new Post(req.body);
 
-	post.save(function(err, post) {
-		if(err){ return next(err); }
+  post.save(function(err, post) {
+    if(err){ return next(err); }
 
-		res.json(post);
-	});
+    res.json(post);
+  });
 });
 
 router.param('post', function(req, res, next, id) {
-	var query = Post.findById(id);
+  var query = Post.findById(id);
 
-	query.exec(function(err, post) {
-		if (err) { return next(err); }
-		if (!post) { return next(new Error('can\'t find post')); }
+  query.exec(function(err, post) {
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
 
-		req.post = post;
-		return next();
-	});
+    req.post = post;
+    return next();
+  });
 });
 
 router.get('/posts/:post', function(req, res, next) {
-	req.post.populate('comments', function(err, post) {
-		if (err) { return next(err); }
+  req.post.populate('comments', function(err, post) {
+    if (err) { return next(err); }
 
-		res.json(post);
-	});
+    res.json(post);
+  });
 });
 
 router.put('/posts/:post/upvote', function(req, res, next) {
-	req.post.upvote(function(err, post) {
-		if (err) { return next(err); }
+  req.post.upvote(function(err, post) {
+    if (err) { return next(err); }
 
-		res.json(post);
-	});
+    res.json(post);
+  });
 });
 
 router.post('/posts/:post/comments', function(req, res, next) {
-	var comment = new Comment(req.body);
-	comment.post = req.post;
+  var comment = new Comment(req.body);
+  comment.post = req.post;
 
-	comment.save(function(err, comment) {
-		if (err) { return next(err); }
+  comment.save(function(err, comment) {
+    if (err) { return next(err); }
 
-		req.post.comments.push(comment);
-		req.post.save(function(err, post) {
-			if(err) { return next(err); }
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err) { return next(err); }
 
-			res.json(comment);
-		});
+      res.json(comment);
+    });
 
-	});
+  });
 });
 
 // Errors in the following router.param and router.put code for comments:
 router.param('comment', function(req, res, next, id) {
-	var query = Comment.findById(id);
+  var query = Comment.findById(id);
 
-	query.exec(function(err, comment) {
-		if (err) { return next(err); }
-		if (!comment) { return next(new Error('can\'t find comment')); }
+  query.exec(function(err, comment) {
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error('can\'t find comment')); }
 
-		req.comment = comment;
-		return next();
-	});
+    req.comment = comment;
+    return next();
+  });
 });
 
 router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
-	req.comment.upvote(function(err, comment) {
-		if (err) { return next(err); }
+  req.comment.upvote(function(err, comment) {
+    if (err) { return next(err); }
 
-		res.json(comment);
-	});
+    res.json(comment);
+  });
 });
