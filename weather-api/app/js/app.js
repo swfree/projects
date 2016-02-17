f9fb8b146e75574158818692236dfb3d9e6f958f
swfree <skye.free@gmail.com>
2016-02-16 20:04:39 -0500

    Add submit function to controller

--- weather-api/app/js/app.js
+++ weather-api/app/js/app.js
@@ -5,8 +5,11 @@ var weatherApp = angular.module('weatherApp', []);
 
 /* Controllers */
 weatherApp.controller('WeatherCtrl', ['$scope', '$http', function($scope, $http) {
-  $http.get('http://api.openweathermap.org/data/2.5/forecast?q=Gaithersburg,us&mode=json&appid=44db6a862fba0b067b1930da0d769e98')
+  $scope.userInput = "Gaithersburg";
+  var url = 'http://api.openweathermap.org/data/2.5/forecast?q=' + $scope.userInput + ',us&mode=json&appid=44db6a862fba0b067b1930da0d769e98';
+  $http.get(url)
     .then(function(data) {
+      $scope.userInput = "Gaithersburg";
       $scope.date = data.data.list[0].dt;
       $scope.description = data.data.list[0].weather[0].description;
       var temp = data.data.list[0].main.temp;
@@ -14,92 +17,24 @@ weatherApp.controller('WeatherCtrl', ['$scope', '$http', function($scope, $http)
     }, function(errMsg){
       console.log(errMsg);
   });
+
+  // ng-submit: repeat $http call
+  // refactor note: remove repetition!
+  $scope.submit = function() {
+    url = 'http://api.openweathermap.org/data/2.5/forecast?q=' + $scope.userInput + ',us&mode=json&appid=44db6a862fba0b067b1930da0d769e98';
+    $http.get(url)
+      .then(function(data) {
+        $scope.date = data.data.list[0].dt;
+        $scope.description = data.data.list[0].weather[0].description;
+        var temp = data.data.list[0].main.temp;
+        $scope.mainTemp = Math.floor(temp * (9/5) - 460);
+      }, function(errMsg){
+        console.log(errMsg);
+    });
+  };
 }]);
 
-
-/* Old static code without Angular */
-/*
-$(document).ready(function(){
-  var updateWeather = function(event) {    
-    var location = event.data.locale.val();
-    var outputList = $(this).closest("div.row").next();
-    var url = 'http://api.openweathermap.org/data/2.5/forecast?q=';
-    var appID = '&mode=json&appid=44db6a862fba0b067b1930da0d769e98';
-    var city = location;
-    var country = 'us';
-    var description = '';
-    var temperature;
-
-    url = url + city + ',' + country + appID;
-
-    // contact API for updated weather
-    $.getJSON(url, function(data){
-      var time = data.list[0].dt;
-      var date = new Date(time*1000);
-      var hours = date.getHours();
-      var minutes = "0" + date.getMinutes();
-      var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-      var month = months[date.getMonth()];
-      var day = date.getDate();
-      var formattedTime = day + ' ' + month + ' ' + hours + ':' + minutes.substr(-2);
-
-      description = data.list[0].weather[0].description;
-      temperature = data.list[0].main.temp;
-      temperature = Math.floor(temperature*(9/5) - 460);
-
-      // put inside callback
-      outputList.find("li#location").html(location);
-      outputList.find("li#time").html(formattedTime);
-      outputList.find("span.temp").html('&nbsp;' + temperature + '&nbsp;F');
-      outputList.find("li#description").html(description);
-    });
-
-    // Change webpage after user input
-    //outputList.find("li#location").html(location);
-    //outputList.find("li.temp").html(temperature);
-    //outputList.find("li#description").html(description);
-    // outputList.find("li#time").html(current time);
-    // outputList.find("li#date").html(current date);
-    // etc
-
-    event.preventDefault();
-  };
-
-  $("form").on("submit", {
-    locale: $(this).find("input:text")
-  }, updateWeather);
-
-});
-
-// Learn to use APIs
-//{"_id":2192362,"name":"Christchurch","country":"NZ","coord":{"lon":172.633331,"lat":-43.533329}}
-
-
-// OpenWeatherMap:
-  // example API call by city name:
-    // api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
-    // api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml
-  // example API call by city id (recommended):
-    // city id list: http://bulk.openweathermap.org/sample/
-    // api.openweathermap.org/data/2.5/forecast?id=524901
-
-  // returns json object:
-    // object.list.dt = time of data forecast (unix UTC - secs since 1/1/1970)
-    // object.list.main.temp = current temp in Kelvin
-    // object.list.weather.main || weather.description
-    //
-    // object.main.temp_min
-    // object.main.temp_max
-    //
-    // if you want to input the conditions icon:
-      // http://openweathermap.org/img/w/10d.png
-      // object.list.weather.icon <<< i.e. 10d or 11n etc.
-
-/* 
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://www.codecademy.com/", false);
-  // Add your code below!
-  xhr.send();
-  console.log(xhr.status);
-  console.log(xhr.statusText);
-*/
+/* Next Steps */
+// Add 5-day forecast
+// Add weather condition icons
+// Add better search functionality
