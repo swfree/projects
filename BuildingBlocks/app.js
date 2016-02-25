440529a009fb0e308c1e41af8d6d67f84c1edf79
swfree <skye.free@gmail.com>
2016-02-24 18:43:48 -0800

    Listing cities on index.html

--- BuildingBlocks/app.js
+++ BuildingBlocks/app.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(request, response) {
-  response.send('OK');
-});
+app.use(express.static('public'));
 
 app.get('/cities', function(request, response) {
-  response.send('OK');
+  var cities = ['Lotopia', 'Caspiana', 'Indigo'];
+  response.json(cities);
 });
 
 module.exports = app;
