958ddd85dd23f581162a05559a57add6877c36cd
swfree <skye.free@gmail.com>
2016-02-24 18:22:29 -0800

    Write second test for /cities path

--- BuildingBlocks/test.js
+++ BuildingBlocks/test.js
@@ -12,3 +12,11 @@ describe('Requests to the root path', function() {
       });
   });
 });
+
+describe('Listing cities on /cities', function() {
+  it('Returns 200 status code', function(done) {
+    request(app)
+      .get('/cities')
+      .expect(200, done)
+  });
+});
